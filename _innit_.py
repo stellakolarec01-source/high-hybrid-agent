0d9881a68a22bf8e7c9caf604f02db00707e781a
La Stella <stellakolarec01@gmail.com>
2025-12-11 03:44:43 +0100

    Update _innit_.py

--- _innit_.py
+++ _innit_.py
@@ -1,1 +1,9 @@
+"TENSION LEVEL":HIGH
+"JAKO,SPORO,POLAKO
+if emotion in ("despair")
+return "gentle direct"
+jasno, bez muljanja
+if emotion =="anger" :
+  return "calm fire"
+  svađu,ali se ne povlači
 
